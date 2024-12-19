9e017e51f9e578eee98602f9a01014efb3c549a7
MO SAIF <mosaif73940@gmail.com>
2024-12-20 01:06:18 +0530

    Second Programme

---
+++ 2.cpp
@@ -0,0 +1,13 @@
+
+//  Hello world Programme . This  is  a  simple  programme  to  print  "Hello,  World
+// Hello World Programme
+// This is a simple programme to print "Hello, World"
+
+
+#include<iostream>
+using namespace std;
+
+int main() {
+    cout << "Hello, World!" << endl;
+    return 0;
+}
